4e13b2375419af9dcf0e34bf23400bd49b8158b3
Tomohiro TAIRA <tomohiro.t@gmail.com>
2016-02-16 19:17:05 +0900

    Add vendor directory to store dependency libraries

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+import sys
+
+# Add vendor directory to module paths
+parent_dir = os.path.abspath(os.path.dirname(__file__))
+vendor_dir = os.path.join(parent_dir, 'vendor')
+sys.path.append(vendor_dir)
